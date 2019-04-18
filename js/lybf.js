24056f75757df5117acf89f785fa59e9043bfcc2
iFaluo <mful6ntp@outlook.com>
2019-04-18 13:41:39 +0800

    恢复Lab链接，指向倒数节点。

--- js/lybf.js
+++ js/lybf.js
@@ -12,7 +12,7 @@ function resize(){
 	$("#main").css("height",h-60);
 	var mainBlockWidth = parseInt($("#main_block").css("width"));
 	var mainBlockHeight = parseInt($("#main_block").css("height"));
-	$("#main_block").css("left",(w-mainBlockWidth)/2);
+	$("#main_block").css("left",(w-mainBlockWidth)/2+60);
 	if (mainBlockHeight < (h-60)){
 		$("#feet").addClass("nail_bottom");
 	} else {
